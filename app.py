3618f040a45d5552e143eadac287c85a696da8f9
tagos777.dev() <tagos777dograu@gmail.com>
2025-11-16 08:41:21 -0300

    Implement task creation route in Flask app
    Added a new route for creating tasks and modified imports.

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
-
+from flask import Flask, request
+from models.task import task  
 app = Flask(__name__)
 
-@app.route("/")
-def heloo_word():
-    return "michel e viado"
-@app.route("/about")
-def about():
-    return "pagina sobre"
+#CRUD:create, read, update and  delete = criar, ler, atulizar, e deletar
+#tabela : tarefa 
+
+tasks = []
+
+
+@app.route("/tasks",methods=["POST"])
+def create_tasks():
+    data = request.get_json()
+    print(data)
+    return "teste"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
